f35c6c89b59f0c7065f8ed6b55a61afcfec1a481
Fernando3643 <fernando.36.43.reyes@gmail.com>
2018-10-18 10:44:43 -0500

    [agregado] proyecto de angular con material instalado

--- angularApplication/src/app/app.module.ts
+++ angularApplication/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
