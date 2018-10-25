64bc77dab85f47143d2dcf7442fe5ff440f7b7af
Fernando3643 <fernando.36.43.reyes@gmail.com>
2018-10-25 10:21:42 -0500

    [agregado] Vista de usuarios

---
+++ angularApplication/src/app/_start/models/student.ts
@@ -0,0 +1,16 @@
+
+
+export class Students {
+    id: number;
+    name: string;
+    lastName: string;
+    grade: string;
+    calification: number;
+    constructor() {
+        this.id=null;
+        this.name='';
+        this.lastName='';
+        this.grade='';
+        this.calification=null;
+    }
+}
--- angularApplication/src/app/users/users.component.ts
+++ angularApplication/src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Students } from '../_start/models/student';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-users-component',
@@ -7,6 +9,20 @@ import { Component} from '@angular/core';
 
 })
 export class UsersComponent {
+  displayedColumns: string[] = ['id', 'name', 'grade','calification'];
+  students: Students[]=[
+    {id:1,name:"Fer",lastName:"Reyes",grade:"5A",calification:10},
+    {id:1,name:"Fer",lastName:"Reyes",grade:"5A",calification:10},
+    {id:1,name:"Fer",lastName:"Reyes",grade:"5A",calification:10},
+    {id:1,name:"Fer",lastName:"Reyes",grade:"5A",calification:10},
+    {id:1,name:"Fer",lastName:"Reyes",grade:"5A",calification:10},
+    
+  ]
+  dataSource = new MatTableDataSource<Students>(this.students);
+
   constructor() {
+  }
+  addUser(){
+    console.log("Tu eres igual o mas lista")
   }
 }
